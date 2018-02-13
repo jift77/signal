ab7be6a99ee0703af549c013af0550b50f107754
jift77 <serviciotecnicotoyota@gmail.com>
2018-02-12 23:33:34 -0500

    first commit

---
+++ signal/EchoHub.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace signal
+{
+    [HubName("Echo")]
+    public class EchoHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();    
+        }
+
+        public void Say(string message)
+        {
+            Trace.WriteLine(message);
+        }
+    }
+}
---
+++ signal/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(signal.Startup))]
+
+namespace signal
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
